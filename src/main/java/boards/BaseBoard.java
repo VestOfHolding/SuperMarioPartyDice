eefa78d0a3688171fbb2f045b3904cdfe00397ca
VestOfHolding <moviestiles@gmail.com>
2018-12-06 20:54:35 -0800

    Redoing some of the function structures.

--- src/main/java/boards/BaseBoard.java
+++ src/main/java/boards/BaseBoard.java
@@ -4,18 +4,17 @@ import boards.layout.MPBoard;
 import boards.spaces.BaseSpace;
 import boards.spaces.events.EventSpace;
 import boards.spaces.events.MoveEventSpace;
-import org.apache.commons.collections4.CollectionUtils;
 import org.jgrapht.Graphs;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.builder.GraphBuilder;
 import stattracker.GameStatTracker;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Random;
-import java.util.stream.Collectors;
 
 public abstract class BaseBoard {
+    protected Random random = new Random();
+
     protected MPBoard<BaseSpace, DefaultEdge> board;
 
     protected GraphBuilder<BaseSpace, DefaultEdge, MPBoard<BaseSpace, DefaultEdge>> graphBuilder;
@@ -27,27 +26,6 @@ public abstract class BaseBoard {
         initializeBoard();
     }
 
-    public List<BaseSpace> travelToNextSpaces(BaseSpace startingSpace) {
-        return travelToNextSpaces(Collections.singletonList(startingSpace));
-    }
-
-    protected List<BaseSpace> travelToNextSpaces(List<BaseSpace> startingSpaces) {
-        if (CollectionUtils.isEmpty(startingSpaces)) {
-            return Collections.emptyList();
-        }
-        try {
-            return startingSpaces.stream()
-                    .flatMap(space -> Graphs.successorListOf(board, space).stream())
-                    .distinct()
-                    .collect(Collectors.toList());
-
-        } catch (NullPointerException npe) {
-            System.out.println("Null pointer on this space: " + String.join(",", startingSpaces.toString()));
-        }
-
-        return Collections.emptyList();
-    }
-
     public BaseSpace getStartSpace() {
         return board.getStartSpace();
     }
@@ -56,19 +34,15 @@ public abstract class BaseBoard {
         return board.getGraphSize();
     }
 
-    public BaseSpace getDestination(BaseSpace startSpace, int distance, GameStatTracker gameStatTracker) {
-        BaseSpace currentSpace = startSpace;
-        Random random = new Random();
+    public BaseSpace getNextSpace(BaseSpace startingSpace) {
+        List<BaseSpace> nextSpaces = Graphs.successorListOf(board, startingSpace);
 
+        return nextSpaces.get(nextSpaces.size() > 1 ? random.nextInt(nextSpaces.size()) : 0);
+    }
+
+    public BaseSpace getDestination(BaseSpace currentSpace, int distance, GameStatTracker gameStatTracker) {
         for (int i = 0; i < distance; ++i) {
-            List<BaseSpace> nextSpaces = travelToNextSpaces(currentSpace);
-
-            if (nextSpaces.size() > 1) {
-                currentSpace = nextSpaces.get(random.nextInt(nextSpaces.size()));
-            }
-            else {
-                currentSpace = nextSpaces.get(0);
-            }
+            currentSpace = getNextSpace(currentSpace);
 
             if (!currentSpace.affectsMovement()) {
                 i -= 1;
@@ -78,7 +52,7 @@ public abstract class BaseBoard {
         if (currentSpace instanceof EventSpace && !currentSpace.isPassingEvent()) {
             if (currentSpace instanceof MoveEventSpace && ((MoveEventSpace) currentSpace).getSpaceToMoveToID() > -1) {
                 MoveEventSpace eventSpace = (MoveEventSpace) currentSpace;
-                currentSpace = board.getVertexById(((MoveEventSpace) currentSpace).getSpaceToMoveToID());
+                currentSpace = board.getVertexById(eventSpace.getSpaceToMoveToID());
                 //This is where the event space gets transformed into a blue space.
                 eventSpace.processEvent(board, gameStatTracker, eventSpace);
             }
