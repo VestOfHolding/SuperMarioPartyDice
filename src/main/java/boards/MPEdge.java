64c41f69ed47af2537842297d6e845493f5c2c9a
VestOfHolding <moviestiles@gmail.com>
2019-03-30 19:42:58 -0700

    Adding edge weight logic override to the edge class too.

--- src/main/java/boards/MPEdge.java
+++ src/main/java/boards/MPEdge.java
@@ -1,6 +1,7 @@
 package boards;
 
 import boards.spaces.BaseSpace;
+import boards.spaces.StarSpace;
 import org.jgrapht.graph.DefaultWeightedEdge;
 
 public class MPEdge extends DefaultWeightedEdge {
@@ -13,6 +14,20 @@ public class MPEdge extends DefaultWeightedEdge {
     @Override
     public BaseSpace getTarget() {
         return (BaseSpace)super.getTarget();
+    }
+
+    /**
+     * Star spaces only don't count towards movement, and therefore don't have an incoming edge weight,
+     *  if the star is currently active, which can obviously change over the course of the game.
+     *
+     *  Therefore, if the target of this edge is a star space, check again if it currently affects movement.
+     */
+    @Override
+    public double getWeight() {
+        double weight = super.getWeight();
+
+        return getTarget() instanceof StarSpace && !getTarget().affectsMovement()
+                ? 0.0 : weight;
     }
 
     public String getSourceName() {
