cc515c8bdcf9a36a4eac2637a9348c9f1700f283
VestOfHolding <moviestiles@gmail.com>
2019-04-16 21:28:45 -0700

    Removing graph visualization.

--- src/main/java/simulation/MainSim.java
+++ src/main/java/simulation/MainSim.java
@@ -5,18 +5,12 @@ import boards.KameksTantalizingTower;
 import boards.KingBobombsPowderkegMine;
 import boards.MegafruitParadise;
 import boards.WhompsDominoRuins;
-import display.GraphDisplay;
 import org.apache.commons.lang3.time.StopWatch;
 
 public class MainSim {
-    public static void main(String[] args) throws Exception {
-        System.setProperty("org.graphstream.ui", "swing");
-
+    public static void main(String[] args) {
         StopWatch stopWatch = StopWatch.createStarted();
         BaseBoard board = new WhompsDominoRuins();
-
-//        GraphDisplay display = new GraphDisplay();
-//        display.display(board);
 
         Simulation simulation = new Simulation(board);
         simulation.simulate();
