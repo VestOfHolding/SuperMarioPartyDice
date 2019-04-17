5f95268260130dc1ede7ecec741224fa256fbb29
VestOfHolding <moviestiles@gmail.com>
2019-04-16 20:53:33 -0700

    More Lombok removal for performance.

--- src/main/java/utils/LuckEvent.java
+++ src/main/java/utils/LuckEvent.java
@@ -2,14 +2,16 @@ package utils;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.ToString;
 import org.apache.commons.lang3.Range;
 
-@Data
+import java.util.Objects;
+
+@Getter
 @Builder
 @AllArgsConstructor
-@ToString(callSuper = true)
+@ToString
 public class LuckEvent {
     //Meeehhhh, I hate this, but the only other thing I can think of right now is to
     // turn this class into another enum, and the idea of LuckyEventTable being
@@ -58,5 +60,22 @@ public class LuckEvent {
 
     public static LuckEvent buildEmptyEvent(Range<Integer> chanceRange) {
         return new LuckEvent(chanceRange);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        LuckEvent luckEvent = (LuckEvent) o;
+        return getChanceRange().equals(luckEvent.getChanceRange());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getChanceRange());
     }
 }
