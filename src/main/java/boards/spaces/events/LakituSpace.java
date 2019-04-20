7e7155376377f4e9b31239e598a5fe94a0dc68d7
VestOfHolding <moviestiles@gmail.com>
2019-04-20 11:09:56 -0700

    Lakitu can steal stars now.

--- src/main/java/boards/spaces/events/LakituSpace.java
+++ src/main/java/boards/spaces/events/LakituSpace.java
@@ -8,8 +8,13 @@ import simulation.Player;
 import simulation.PlayerGroup;
 import utils.RandomUtils;
 
+import java.util.Comparator;
+
 @ToString(callSuper = true)
 public class LakituSpace extends EventSpace {
+
+    @ToString.Exclude
+    private static final int STAR_STEAL_COST = 30;
 
     public LakituSpace(int spaceID, int x, int y) {
         super(spaceID, x, y);
@@ -23,10 +28,31 @@ public class LakituSpace extends EventSpace {
     @Override
     public boolean processEvent(MPBoard<BaseSpace, MPEdge> gameBoard,
                                 Player currentPlayer, PlayerGroup playerGroup) {
-        int stolenCoinAmount = playerGroup.getRandomPlayerBesidesCurrent(currentPlayer)
-                .takeCoins(RandomUtils.getRandomInt(5, 10));
+        //If we have the coins, steal a star.
+        if (currentPlayer.getCoinTotal() >= STAR_STEAL_COST + gameBoard.getStarCost()
+                //Oh right, and the opponents should actually have stars to steal.
+                && playerGroup.getAllPlayersExceptCurrent(currentPlayer).stream()
+                .anyMatch(p -> p.getStarCount() > 0)) {
 
-        currentPlayer.addCoins(stolenCoinAmount);
+            //Ok, we're definitely stealing a star now. Steal from the person in first place/has the most stars.
+            Player victim = playerGroup.getAllPlayersExceptCurrent(currentPlayer).stream()
+                    .max(Comparator.comparing(Player::getCurrentPlace))
+                    .orElse(null);
+            
+            if (victim != null) {
+                victim.loseStar();
+                currentPlayer.addStar();
+                currentPlayer.addCoins(-STAR_STEAL_COST);
+            }
+        }
+        //Otherwise, steal coins at no cost.
+        else {
+            int stolenCoinAmount = playerGroup.getRandomPlayerBesidesCurrent(currentPlayer)
+                    .takeCoins(RandomUtils.getRandomInt(5, 10));
+
+            currentPlayer.addCoins(stolenCoinAmount);
+        }
+
         return true;
     }
 }
