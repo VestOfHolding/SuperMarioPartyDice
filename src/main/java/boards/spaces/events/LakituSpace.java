0309567d3a1a5be8a9331013e66b27859ca5ffb2
VestOfHolding <moviestiles@gmail.com>
2019-04-20 10:52:15 -0700

    Implemented Lakitu space stealing coins.

--- src/main/java/boards/spaces/events/LakituSpace.java
+++ src/main/java/boards/spaces/events/LakituSpace.java
@@ -6,6 +6,7 @@ import boards.spaces.BaseSpace;
 import lombok.ToString;
 import simulation.Player;
 import simulation.PlayerGroup;
+import utils.RandomUtils;
 
 @ToString(callSuper = true)
 public class LakituSpace extends EventSpace {
@@ -22,8 +23,10 @@ public class LakituSpace extends EventSpace {
     @Override
     public boolean processEvent(MPBoard<BaseSpace, MPEdge> gameBoard,
                                 Player currentPlayer, PlayerGroup playerGroup) {
-//        gameStatTracker.addCoins(RandomUtils.getRandomInt(5, 10));
+        int stolenCoinAmount = playerGroup.getRandomPlayerBesidesCurrent(currentPlayer)
+                .takeCoins(RandomUtils.getRandomInt(5, 10));
+
+        currentPlayer.addCoins(stolenCoinAmount);
         return true;
     }
-
 }
